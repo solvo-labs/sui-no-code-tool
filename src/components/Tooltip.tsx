1f98800cad1715d2979c891e3ed443812abd22f3
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-12-21 17:53:49 +0300

    fixed location

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -9,17 +9,17 @@ const Tooltip: React.FC<Props> = ({ title, isOpen, position }) => {
   return (
     <>
       {isOpen && position === "bottom" && (
-        <div className="flex absolute text-white bottom-[380px] left-[470px] w-72">
+        <div className="flex absolute text-white w-72 top-12 left-[-10px]">
           <svg className="absolute left-4 text-black h-4 bottom-0" viewBox="0 0 255 255">
             <polygon className="fill-current" points="0,255 127.5,127.5 255,255" />
           </svg>
-          <div className="absolute flex bg-gray-900 top-0 left-0 min-w-48 max-w-72 shadow-xl px-4 py-2 rounded-xl">
+          <div className="absolute flex bg-gray-900 top-0 left-0 max-w-72 shadow-xl px-4 py-2 rounded-xl">
             <p className="flex w-full">{title}</p>
           </div>
         </div>
       )}
       {isOpen && position === "top" && (
-        <div className="flex absolute text-white top-[520px] left-[470px] w-72">
+        <div className="flex absolute text-white w-72 bottom-12 left-[-10px]">
           <svg className="absolute left-4 text-black h-4 top-0" viewBox="0 0 255 255">
             <polygon className="fill-current" points="255,0 127.5,127.5 0,0" />
           </svg>
