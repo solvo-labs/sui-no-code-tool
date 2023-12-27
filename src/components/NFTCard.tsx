16d3d57ea2e84258080479dc68b7809b09c25693
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-12-27 12:32:23 +0300

    fixed

--- src/components/NFTCard.tsx
+++ src/components/NFTCard.tsx
@@ -10,7 +10,7 @@ const NFTCard: React.FC<Props> = ({ nft }) => {
   return (
     <div className="nft-card" key={nft.data.objectId}>
       <div className="flex flexdir-col items-center gap-4">
-        {nft.data.content.fields.url == "" ? (
+        {!nft.data.content.fields.url ? (
           <div className="flex items-center justify-center border-2 border-gray-300 w-[100px] h-[100px] rounded-full bg-gray-100">
             <MdInsertPhoto style={{ color: "gray" }}></MdInsertPhoto>
           </div>
