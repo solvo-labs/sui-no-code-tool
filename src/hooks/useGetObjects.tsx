9a5e4c5dce56b81f9c526d05f5a3ba7fc8dcf72e
afarukcali <afarukcali@gmail.com>
2023-12-27 12:56:19 +0300

    fetch nft review.

--- src/hooks/useGetObjects.tsx
+++ src/hooks/useGetObjects.tsx
@@ -13,41 +13,71 @@ export default function useGetObjects(wallet: WalletAccount) {
 
   useEffect(() => {
     const init = async () => {
-      const objects = await suiClient.getOwnedObjects({ owner: wallet.address });
-      const resultAllObjects = objects.data;
-
-      const promiseData = resultAllObjects.map((obj: any) =>
-        suiClient.getObject({
-          id: obj.data.objectId,
-          options: {
-            showContent: true,
-          },
-        })
-      );
-      const allObjects = await Promise.all(promiseData);
-      setObjects(allObjects);
-
-      const nftObjects: any[] = [];
-      const coinObjects: any[] = [];
-
-      allObjects.forEach((fr: any) => {
-        if (fr.data) {
-          const type = fr.data.content?.type;
-
-          if (type === "0x44d12155bb085df7d5432f0ad2419eb46195c449c327c716f43b733cfd17884d::devnet_nft::DevNetNFT") {
-            nftObjects.push(fr);
-          }
-
-          if (type.startsWith("0x2::coin::") && type !== "0x2::coin::Coin<0x2::sui::SUI>") {
-            const isExist = coinObjects.findIndex((cb) => cb.data.content?.type === type);
-
-            if (isExist < 0) coinObjects.push(fr);
-          }
-        }
+      const nftObjects = await suiClient.getOwnedObjects({
+        owner: wallet.address,
+        filter: {
+          MatchAll: [
+            {
+              StructType: "0x44d12155bb085df7d5432f0ad2419eb46195c449c327c716f43b733cfd17884d::devnet_nft::DevNetNFT",
+            },
+          ],
+        },
+        options: {
+          showContent: true,
+        },
       });
 
-      setNfts(nftObjects);
-      setCoins(coinObjects);
+      setNfts(nftObjects.data as any);
+
+      // const coinObjects = await suiClient.getOwnedObjects({
+      //   owner: wallet.address,
+      //   limit: 50,
+      //   filter: {
+      //     MatchAll: [
+      //       {
+      //         StructType: type.startsWith("0x2::coin::") && type !== "0x2::coin::Coin<0x2::sui::SUI>",
+      //       },
+      //     ],
+      //   },
+      //   options: {
+      //     showContent: true,
+      //   },
+      // });
+
+      // const resultAllObjects = objects.data;
+
+      // const promiseData = resultAllObjects.map((obj: any) =>
+      //   suiClient.getObject({
+      //     id: obj.data.objectId,
+
+      //     options: {
+      //       showContent: true,
+      //     },
+      //   })
+      // );
+      // const allObjects = await Promise.all(promiseData);
+      // setObjects(allObjects);
+
+      // const nftObjects: any[] = [];
+      // const coinObjects: any[] = [];
+
+      // allObjects.forEach((fr: any) => {
+      //   if (fr.data) {
+      //     const type = fr.data.content?.type;
+
+      //     if (type === "0x44d12155bb085df7d5432f0ad2419eb46195c449c327c716f43b733cfd17884d::devnet_nft::DevNetNFT") {
+      //       nftObjects.push(fr);
+      //     }
+
+      //     if (type.startsWith("0x2::coin::") && type !== "0x2::coin::Coin<0x2::sui::SUI>") {
+      //       const isExist = coinObjects.findIndex((cb) => cb.data.content?.type === type);
+
+      //       if (isExist < 0) coinObjects.push(fr);
+      //     }
+      //   }
+      // });
+
+      setCoins([]);
 
       setLoading(false);
     };
