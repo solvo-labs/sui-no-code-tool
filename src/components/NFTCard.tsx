238f24e66d407575e940b9bf2f57f358470a3f01
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2023-12-28 03:20:55 +0300

    nft card navigate added

--- src/components/NFTCard.tsx
+++ src/components/NFTCard.tsx
@@ -4,22 +4,23 @@ import "../index.css";
 
 type Props = {
   nft: NftObject;
+  onClick: () => void;
 };
 
-const NFTCard: React.FC<Props> = ({ nft }) => {
+const NFTCard: React.FC<Props> = ({ nft, onClick }) => {
   return (
-    <div className="nft-card" key={nft.data.objectId}>
+    <div className="nft-card" key={nft.data.objectId} onClick={onClick}>
       <div className="flex flexdir-col items-center gap-4">
-        {!nft.data.content.fields.url ? (
+        {nft.data.content?.fields.url ? (
+          <img className="w-[100px] h-[100px] rounded-full" src={nft.data.content.fields.url} alt={nft.data.content.fields.name}></img>
+        ) : (
           <div className="flex items-center justify-center border-2 border-gray-300 w-[100px] h-[100px] rounded-full bg-gray-100">
             <MdInsertPhoto style={{ color: "gray" }}></MdInsertPhoto>
           </div>
-        ) : (
-          <img className="w-[100px] h-[100px] rounded-full" src={nft.data.content.fields.url} alt={nft.data.content.fields.name}></img>
         )}
         <div className="flex flex-col">
-          <p className="font-bold text-xl">{nft.data.content?.fields.name}</p>
-          <p>{nft.data.content?.fields.description}</p>
+          <p className="font-bold text-xl">{nft.data.content.fields.name}</p>
+          <p>{nft.data.content.fields.description}</p>
           <p>{nft.data.objectId.slice(0, 5) + "..." + nft.data.objectId.slice(-5)} </p>
         </div>
       </div>
