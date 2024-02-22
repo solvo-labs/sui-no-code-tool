4f440c44b252dc5603f3ca4415df64d84bf2de79
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2024-02-22 13:51:05 +0300

    sui balance hook added to main page

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useCurrentAccount } from "@mysten/dapp-kit";
+import { SuiClient } from "@mysten/sui.js/client";
+import { useOutletContext } from "react-router-dom";
+import useGetSuiBalance from "../hooks/useGetSuiBalance";
 
 const Main = () => {
-  return <div>Main</div>;
+  const account = useCurrentAccount();
+  const [suiClient] = useOutletContext<[suiClient: SuiClient]>();
+  const { suiBalance } = useGetSuiBalance(account!, suiClient);
+
+  return (
+    <div>
+      <p>Total Balance: {suiBalance ? Number(suiBalance?.totalBalance) / Math.pow(10, 9) : "0"}</p>
+    </div>
+  );
 };
 
 export default Main;
