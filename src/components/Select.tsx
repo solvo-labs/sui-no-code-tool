4c8b1d5145cabe24eaa523e82a67ca67743ce927
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2024-02-22 12:22:52 +0300

    so important bug fixed

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -15,7 +15,7 @@ type Props = {
 
 export const Select: React.FC<Props> = ({ title, options, selectedOption, onSelect, placeholder = "Select an option" }) => {
   return (
-    <div className="relative inline-flex w-full  flex-col">
+    <div className="relative inline-flex w-full flex-col">
       <label className="mb-2 text-sm font-medium text-gray-900">{title}</label>
       <select
         className="border border-gray-300 rounded-lg cursor-pointer text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none"
