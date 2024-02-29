b6f159c2528a70fa52d4fec77bb07e933a58e125
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2024-02-29 16:24:54 +0300

    btn right and left icon props added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,16 +4,20 @@ type Props = {
   title: string;
   disabled?: boolean;
   onClick: () => void;
+  leftImage?: any;
+  rightImage?: any;
 };
 
-const Button: React.FC<Props> = ({ title, disabled, onClick }) => {
+const Button: React.FC<Props> = ({ title, disabled, rightImage, leftImage, onClick }) => {
   return (
     <button
       disabled={disabled}
-      className="rounded-sui-radius bg-sui-blue text-white w-full disabled:bg-sui-blue-d disabled:border disabled:border-sui-blue-d disabled:cursor-not-allowed hover:bg-sui-blue-h"
+      className="flex flex-row justify-center rounded-sui-radius bg-sui-blue text-white w-full disabled:bg-sui-blue-d disabled:border disabled:border-sui-blue-d disabled:cursor-not-allowed hover:bg-sui-blue-h"
       onClick={onClick}
     >
+      <div className="pr-2">{leftImage}</div>
       <p className="text-[16px] font-bold">{title}</p>
+      <div className="pl-2">{rightImage}</div>
     </button>
   );
 };
