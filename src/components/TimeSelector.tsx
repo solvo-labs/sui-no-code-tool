06bc16cef63b54ae670999d254da137f5cf00f71
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2024-03-01 18:12:19 +0300

    added time input

--- src/components/TimeSelector.tsx
+++ src/components/TimeSelector.tsx
@@ -1,11 +1,14 @@
+import moment from "moment";
 import React, { ChangeEvent } from "react";
 
 type Props = {
-  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleDate: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleTime: (e: ChangeEvent<HTMLInputElement>) => void;
   title?: string;
+  date?: number;
 };
 
-const TimeSelector: React.FC<Props> = ({ title = "Select Date", handleChange }) => {
+const TimeSelector: React.FC<Props> = ({ title = "Select Date", date, handleDate, handleTime }) => {
   return (
     <div className="relative max-w-sm">
       <label className="block mb-2 text-sm font-medium text-gray-900">{title}</label>
@@ -14,12 +17,22 @@ const TimeSelector: React.FC<Props> = ({ title = "Select Date", handleChange }) 
           <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
         </svg> */}
       </div>
-      <input
-        onChange={handleChange}
-        type="date"
-        className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-sui-blue hover:bg-gray-50 hover:border-gray-400 block w-full ps-4 p-2.5"
-        placeholder="Select date"
-      />
+      <div className="flex">
+        <input
+          type="date"
+          value={moment(date).format("YYYY-MM-DD")}
+          onChange={handleDate}
+          placeholder="Select date"
+          className="w-1/2 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-sui-blue hover:bg-gray-50 hover:border-gray-400 block w-full mr-1 ps-4 p-2.5"
+        />
+        <input
+          type="time"
+          value={moment(date).format("HH:mm")}
+          onChange={handleTime}
+          placeholder="Select time"
+          className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-sui-blue hover:bg-gray-50 hover:border-gray-400 block w-full ml-1 ps-4 p-2.5"
+        ></input>
+      </div>
     </div>
   );
 };
