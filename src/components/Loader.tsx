755abddee881e6fef39e413bdcf8c1ea652dd387
Oguzhan Iptes <oguzhaniptes02@gmail.com>
2024-04-10 14:56:37 +0300

    laoder fixed

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@ import Loading from "../assets/loading-spinner.gif";
 
 export const Loader = () => {
   return (
-    <div className="min-h-dvh relative flex flex-col justify-center items-center">
+    <div className="min-h-full relative flex flex-col justify-center items-center">
       <img className="w-14 h-14" src={Loading} alt="Loading Spinner" />
     </div>
   );
